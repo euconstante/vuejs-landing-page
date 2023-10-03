9bbab2eff09dac35a9dfd86e53b0ca8a090cc891
euconstante <constanteguedert@gmail.com>
2023-10-03 12:34:46 -0300

    fix: add target blank to footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -8,11 +8,13 @@ import '../assets/styles/components/TheFooter.css'
       <p>
         © 2023 JustWatch - The Streaming Guide - - Personalized cover letter for JustWatch Frontend Developer position.
         Built with
-        <a class="link" href="https://vuejs.org/">Vue</a>,
-        <a class="link" href="https://www.typescriptlang.org/">TypeScript</a> and <span>&#128155;</span> by
-        <a class="link" href="https://www.linkedin.com/in/biancaguedert/">Bianca Guedert</a>. <br /><a
+        <a class="link" href="https://vuejs.org/" target="_blank">Vue</a>,
+        <a class="link" href="https://www.typescriptlang.org/" target="_blank">TypeScript</a> and
+        <span>&#128155;</span> by
+        <a class="link" href="https://www.linkedin.com/in/biancaguedert/" target="_blank">Bianca Guedert</a>. <br /><a
           class="link"
           href="https://github.com/euconstante/vuejs-landing-page"
+          target="_blank"
           >Source Code</a
         >
       </p>
